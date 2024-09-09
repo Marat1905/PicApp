a39263c1495b2cb32855dfe0b0d161c5229a07d2
Marat <milya010790@mail.ru>
2024-09-09 15:58:52 +0500

    Получение изображений из галереи

---
+++ PicApp/PicApp/Models/PictureInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PicApp.Models
+{
+    /// <summary>Информация о файле </summary>
+    public class PictureInfo
+    {
+        /// <summary>Имя файла</summary>
+        public string NameFile { get; set; }
+
+        /// <summary>Путь к файлу</summary>
+        public string PathToPicture {  get; set; }
+
+        /// <summary>Дата создания файла</summary>
+        public DateTime CreateDate { get; set; }
+
+        public PictureInfo(string nameFile, string pathToPicture, DateTime createDate)
+        {
+            NameFile = nameFile;
+            PathToPicture = pathToPicture;
+            CreateDate = createDate;
+        }
+    }
+}
--- PicApp/PicApp/ViewModels/GalleryViewModel.cs
+++ PicApp/PicApp/ViewModels/GalleryViewModel.cs
@@ -1,16 +1,57 @@
-﻿using PicApp.Services;
+﻿using PicApp.Models;
+using PicApp.Services;
+using PicApp.ViewModels.Base;
+using System.Collections.ObjectModel;
 using System.IO;
 using Xamarin.Forms;
+using Xamarin.Forms.Shapes;
 
 namespace PicApp.ViewModels
 {
-    internal class GalleryViewModel
+    internal class GalleryViewModel : BaseViewModel
     {
+        /// <summary>Путь к каталогу </summary>
+        private readonly string picturesPath;
+
+        /// <summary>Коллекция изображений</summary>
+        private ObservableCollection<PictureInfo> _pictureList = new ObservableCollection<PictureInfo>();
+
+        /// <summary>Коллекция изображений</summary>
+        public ObservableCollection<PictureInfo> PictureList
+        {
+            get { return _pictureList; }
+            set { SetProperty(ref _pictureList, value); }
+        }
+
+
+
         public GalleryViewModel()
         {
+            Title = "Галерея";
             IPathProvider pathProvider = DependencyService.Get<IPathProvider>();
-            string picturesPath = pathProvider.GetPicturesFolderPath();
+            picturesPath = pathProvider.GetPicturesFolderPath();
             bool isExist = Directory.Exists(picturesPath);
+            InitializeData();
+        }
+
+        private void InitializeData()
+        {
+            LoadPictureList();
+        }
+
+        private async void LoadPictureList()
+        {
+           if(string.IsNullOrEmpty(picturesPath) || !Directory.Exists(picturesPath))
+                return;
+
+            DirectoryInfo dir = new DirectoryInfo(picturesPath);
+
+            var files = dir.GetFileSystemInfos("*.jpg");
+
+            foreach (var file in files)
+            {
+                _pictureList.Add(new PictureInfo(file.Name, file.FullName, file.CreationTime));
+            }
         }
     }
 }
